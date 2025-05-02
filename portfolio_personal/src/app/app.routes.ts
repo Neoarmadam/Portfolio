2d6f4b7b5cc23c2108b6c4c2ae16cc59fb807ab7
Neo Armada <Neoarmadam@gmail.com>
2025-05-02 20:30:41 +0200

    Crear informacion.
    Se ha creado y diseñado el componente de informacion.

--- portfolio_personal/src/app/app.routes.ts
+++ portfolio_personal/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
 import { ProyectosComponent } from './proyectos/proyectos.component';
+import { InformacionComponent } from './informacion/informacion.component';
 
 export const routes: Routes = [
     {
@@ -10,5 +11,9 @@ export const routes: Routes = [
     {
         path: "proyectos",
         component: ProyectosComponent
+    },
+    {
+        path: "informacion",
+        component: InformacionComponent
     }
 ];
