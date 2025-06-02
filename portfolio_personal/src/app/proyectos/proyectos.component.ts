cf60f1ce2872719bb181fed60a2cfc0455ba842b
Neo Armada <Neoarmadam@gmail.com>
2025-06-02 13:14:00 +0200

    Formato mejorado
    Se ha mejorado el formato de todo el proyecto, para que sea mas legible, incluida la tabulacion de los ficheros

--- portfolio_personal/src/app/proyectos/proyectos.component.ts
+++ portfolio_personal/src/app/proyectos/proyectos.component.ts
@@ -2,22 +2,21 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-proyectos',
-  imports: [],
-  templateUrl: './proyectos.component.html',
-  styleUrl: './proyectos.component.css'
+    selector: 'app-proyectos',
+    imports: [],
+    templateUrl: './proyectos.component.html',
+    styleUrl: './proyectos.component.css'
 })
 export class ProyectosComponent implements OnInit{
+    proyectos: any[] = [];
 
-  proyectos: any[] = [];
+    constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) {}
-
-  ngOnInit(): void {
-    this.http.get<any[]>('https://api.github.com/users/neoarmadam/repos')
-      .subscribe(data => {
-        this.proyectos = data.filter(repo => !repo.fork);
-      });
-  }
+    ngOnInit(): void {
+        this.http.get<any[]>('https://api.github.com/users/neoarmadam/repos')
+        .subscribe(data => {
+            this.proyectos = data.filter(repo => !repo.fork);
+        });
+    }
 
 }
